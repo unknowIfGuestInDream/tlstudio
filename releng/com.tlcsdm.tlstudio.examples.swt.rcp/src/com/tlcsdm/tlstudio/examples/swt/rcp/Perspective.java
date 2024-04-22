cc36b1fd008a5d05dbad73325b0b9f0216105a04
梦里不知身是客 <liang.tang.cx@gmail.com>
2024-04-22 21:32:19 +0800

    feat: Add rcp (#28)
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ releng/com.tlcsdm.tlstudio.examples.swt.rcp/src/com/tlcsdm/tlstudio/examples/swt/rcp/Perspective.java
@@ -0,0 +1,15 @@
+package com.tlcsdm.tlstudio.examples.swt.rcp;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+		
+	}
+
+}

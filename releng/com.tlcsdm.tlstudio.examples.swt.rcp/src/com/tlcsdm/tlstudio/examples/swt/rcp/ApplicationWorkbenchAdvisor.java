28e189bb6283097cbd95fd7ac31721cd69fc5881
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2024-07-11 19:28:06 +0800

    feat: Add .classpath

--- releng/com.tlcsdm.tlstudio.examples.swt.rcp/src/com/tlcsdm/tlstudio/examples/swt/rcp/ApplicationWorkbenchAdvisor.java
+++ releng/com.tlcsdm.tlstudio.examples.swt.rcp/src/com/tlcsdm/tlstudio/examples/swt/rcp/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,9 @@
 package com.tlcsdm.tlstudio.examples.swt.rcp;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -9,14 +13,20 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	private static final String PERSPECTIVE_ID = "com.tlcsdm.tlstudio.examples.swt.rcp.perspective";
 
 	@Override
-	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
-			IWorkbenchWindowConfigurer configurer) {
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
 		return new ApplicationWorkbenchWindowAdvisor(configurer);
 	}
-	
+
 	@Override
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
+	}
+
+	@Override
+	public void preStartup() {
+		Location instanceLocation = Platform.getInstanceLocation();
+		URL path = instanceLocation.getURL();
+		// System.out.println(path);
 	}
 
 }

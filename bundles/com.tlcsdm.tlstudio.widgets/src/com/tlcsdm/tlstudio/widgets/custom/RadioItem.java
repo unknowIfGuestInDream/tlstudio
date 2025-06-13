ce506cf59c6bfeba5289a8fa74f256a5298c0e2e
梦里不知身是客 <liang.tang.cx@gmail.com>
2025-06-13 22:34:13 +0800

    feat: Add radioGroup (#318)
    CLose #304

---
+++ bundles/com.tlcsdm.tlstudio.widgets/src/com/tlcsdm/tlstudio/widgets/custom/RadioItem.java
@@ -0,0 +1,421 @@
+package com.tlcsdm.tlstudio.widgets.custom;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Item;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Widget;
+
+/**
+ * Instances of this class represent a selectable user interface object that
+ * represents an radio button in a radio group.
+ * <dl>
+ * <dt><b>Styles:</b></dt>
+ * <dd>LEFT, CENTER, RIGHT</dd>
+ * <dt><b>Events:</b></dt>
+ * <dd>(none)</dd>
+ * </dl>
+ * <p>
+ * Note: Only one of the styles LEFT, RIGHT, and CENTER may be specified.
+ * </p>
+ * <p>
+ * IMPORTANT: This class is <em>not</em> intended to be subclassed.
+ * </p>
+ *
+ * @noextend This class is not intended to be subclassed.
+ */
+public class RadioItem extends Item {
+
+	private static int checkIndex(RadioGroup parent, int position) {
+		if (position == -1) {
+			return parent.getItemCount();
+		}
+		if (position < 0 || position > parent.getItemCount()) {
+			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+		}
+		return position;
+	}
+
+	private static int checkStyle(int style) {
+		int result = 0;
+		if ((style & SWT.LEFT) != 0) {
+			result |= SWT.LEFT;
+		} else if ((style & SWT.CENTER) != 0) {
+			result |= SWT.CENTER;
+		} else if ((style & SWT.RIGHT) != 0) {
+			result |= SWT.RIGHT;
+		}
+		return result;
+	}
+
+	private RadioGroup parent;
+
+	private Button button;
+
+	/**
+	 * Constructs a new instance of this class given its parent and a style value
+	 * describing its behavior and appearance. The item is added to the end of the
+	 * items maintained by its parent.
+	 * <p>
+	 * The style value is either one of the style constants defined in class
+	 * <code>SWT</code> which is applicable to instances of this class, or must be
+	 * built by <em>bitwise OR</em>'ing together (that is, using the
+	 * <code>int</code> "|" operator) two or more of those <code>SWT</code> style
+	 * constants. The class description lists the style constants that are
+	 * applicable to the class. Style bits are also inherited from superclasses.
+	 * </p>
+	 *
+	 * @param parent a widget which will be the parent of the new instance (cannot
+	 *               be null)
+	 * @param style  the style of item to construct
+	 *
+	 * @exception IllegalArgumentException
+	 *                                     <ul>
+	 *                                     <li>ERROR_NULL_ARGUMENT - if the parent
+	 *                                     is null</li>
+	 *                                     </ul>
+	 * @exception SWTException
+	 *                                     <ul>
+	 *                                     <li>ERROR_THREAD_INVALID_ACCESS - if not
+	 *                                     called from the thread that created the
+	 *                                     parent</li>
+	 *                                     </ul>
+	 *
+	 * @see SWT
+	 * @see Widget#getStyle
+	 */
+	public RadioItem(RadioGroup parent, int style) {
+		this(parent, style, -1);
+	}
+
+	/**
+	 * Constructs a new instance of this class given its parent and a style value
+	 * describing its behavior and appearance, and the index at which to place it in
+	 * the items maintained by its parent.
+	 * <p>
+	 * The style value is either one of the style constants defined in class
+	 * <code>SWT</code> which is applicable to instances of this class, or must be
+	 * built by <em>bitwise OR</em>'ing together (that is, using the
+	 * <code>int</code> "|" operator) two or more of those <code>SWT</code> style
+	 * constants. The class description lists the style constants that are
+	 * applicable to the class. Style bits are also inherited from superclasses.
+	 * </p>
+	 *
+	 * @param parent a widget which will be the parent of the new instance (cannot
+	 *               be null)
+	 * @param style  the style of item to construct
+	 * @param index  the zero-relative index at which to store the receiver in its
+	 *               parent
+	 *
+	 * @exception IllegalArgumentException
+	 *                                     <ul>
+	 *                                     <li>ERROR_NULL_ARGUMENT - if the parent
+	 *                                     is null</li>
+	 *                                     <li>ERROR_INVALID_RANGE - if the index is
+	 *                                     not between 0 and the number of elements
+	 *                                     in the parent (inclusive)</li>
+	 *                                     </ul>
+	 * @exception SWTException
+	 *                                     <ul>
+	 *                                     <li>ERROR_THREAD_INVALID_ACCESS - if not
+	 *                                     called from the thread that created the
+	 *                                     parent</li>
+	 *                                     </ul>
+	 *
+	 * @see SWT
+	 * @see Widget#getStyle
+	 */
+	public RadioItem(final RadioGroup parent, int style, int index) {
+		super(parent, checkStyle(style), checkIndex(parent, index));
+		this.parent = parent;
+		button = parent.createButton(getStyle(), index);
+
+		final Listener listener = event -> {
+			if (event.type == SWT.Selection) {
+				handleSelection(event);
+			} else if (event.type == SWT.Dispose) {
+				handleDispose(event);
+			}
+		};
+		button.addListener(SWT.Selection, listener);
+		addListener(SWT.Dispose, listener);
+
+		parent.addItem(this, index);
+	}
+
+	void clear() {
+		setText("");
+		setImage(null);
+		setFont(parent.getFont());
+		setForeground(parent.getForeground());
+		setBackground(parent.getBackground());
+	}
+
+	void deselect() {
+		button.setSelection(false);
+		parent.itemSelected(this);
+	}
+
+	/**
+	 * Returns the receiver's background color.
+	 * <p>
+	 * Note: This operation is a hint and may be overridden by the platform. For
+	 * example, on some versions of Windows the background of a TabFolder, is a
+	 * gradient rather than a solid color.
+	 * </p>
+	 * 
+	 * @return the background color
+	 *
+	 * @exception SWTException
+	 *                         <ul>
+	 *                         <li>ERROR_WIDGET_DISPOSED - if the receiver has been
+	 *                         disposed</li>
+	 *                         <li>ERROR_THREAD_INVALID_ACCESS - if not called from
+	 *                         the thread that created the receiver</li>
+	 *                         </ul>
+	 */
+	public Color getBackground() {
+		checkWidget();
+		return button.getBackground();
+	}
+
+	/**
+	 * @return the button associated of this current item
+	 */
+	public Button getButton() {
+		return button;
+	}
+
+	/**
+	 * Returns the font that the receiver will use to paint textual information.
+	 *
+	 * @return the receiver's font
+	 *
+	 * @exception SWTException
+	 *                         <ul>
+	 *                         <li>ERROR_WIDGET_DISPOSED - if the receiver has been
+	 *                         disposed</li>
+	 *                         <li>ERROR_THREAD_INVALID_ACCESS - if not called from
+	 *                         the thread that created the receiver</li>
+	 *                         </ul>
+	 */
+	public Font getFont() {
+		checkWidget();
+		return button.getFont();
+	}
+
+	/**
+	 * Returns the foreground color that the receiver will use to draw.
+	 *
+	 * @return the receiver's foreground color
+	 *
+	 * @exception SWTException
+	 *                         <ul>
+	 *                         <li>ERROR_WIDGET_DISPOSED - if the receiver has been
+	 *                         disposed</li>
+	 *                         <li>ERROR_THREAD_INVALID_ACCESS - if not called from
+	 *                         the thread that created the receiver</li>
+	 *                         </ul>
+	 */
+	public Color getForeground() {
+		checkWidget();
+		return button.getForeground();
+	}
+
+	/**
+	 * @see org.eclipse.swt.widgets.Item#getImage()
+	 */
+	@Override
+	public Image getImage() {
+		checkWidget();
+		return button.getImage();
+	}
+
+	/**
+	 * Returns the receiver's RadioGroup parent.
+	 *
+	 * @return the receiver's image
+	 *
+	 * @exception SWTException
+	 *                         <ul>
+	 *                         <li>ERROR_WIDGET_DISPOSED - if the receiver has been
+	 *                         disposed</li>
+	 *                         <li>ERROR_THREAD_INVALID_ACCESS - if not called from
+	 *                         the thread that created the receiver</li>
+	 *                         </ul>
+	 */
+	public RadioGroup getParent() {
+		return parent;
+	}
+
+	/**
+	 * @see org.eclipse.swt.widgets.Item#getText()
+	 */
+	@Override
+	public String getText() {
+		checkWidget();
+		return button.getText();
+	}
+
+	private void handleDispose(Event event) {
+		if (parent != null) {
+			parent.removeItem(RadioItem.this);
+		}
+		if (button != null) {
+			button.dispose();
+			if (parent != null && !parent.isDisposed()) {
+				parent.layout(false);
+			}
+		}
+		RadioItem.this.parent = null;
+		button = null;
+	}
+
+	private void handleSelection(Event event) {
+		parent.itemSelected(this);
+	}
+
+	/**
+	 * Returns <code>true</code> if the receiver is selected, and false otherwise.
+	 * <p>
+	 * When the receiver is of type <code>CHECK</code> or <code>RADIO</code>, it is
+	 * selected when it is checked. When it is of type <code>TOGGLE</code>, it is
+	 * selected when it is pushed in. If the receiver is of any other type, this
+	 * method returns false.
+	 *
+	 * @return the selection state
+	 *
+	 * @exception SWTException
+	 *                         <ul>
+	 *                         <li>ERROR_WIDGET_DISPOSED - if the receiver has been
+	 *                         disposed</li>
+	 *                         <li>ERROR_THREAD_INVALID_ACCESS - if not called from
+	 *                         the thread that created the receiver</li>
+	 *                         </ul>
+	 */
+	public boolean isSelected() {
+		return button.getSelection();
+	}
+
+	void select() {
+		button.setSelection(true);
+		parent.itemSelected(this);
+	}
+
+	/**
+	 * Sets the receiver's background color to the color specified by the argument,
+	 * or to the default system color for the control if the argument is null.
+	 * <p>
+	 * Note: This operation is a hint and may be overridden by the platform.
+	 * </p>
+	 * 
+	 * @param color the new color (or null)
+	 *
+	 * @exception IllegalArgumentException
+	 *                                     <ul>
+	 *                                     <li>ERROR_INVALID_ARGUMENT - if the
+	 *                                     argument has been disposed</li>
+	 *                                     </ul>
+	 * @exception SWTException
+	 *                                     <ul>
+	 *                                     <li>ERROR_WIDGET_DISPOSED - if the
+	 *                                     receiver has been disposed</li>
+	 *                                     <li>ERROR_THREAD_INVALID_ACCESS - if not
+	 *                                     called from the thread that created the
+	 *                                     receiver</li>
+	 *                                     </ul>
+	 */
+	public void setBackground(Color background) {
+		checkWidget();
+		if (background == null) {
+			SWT.error(SWT.ERROR_NULL_ARGUMENT);
+		}
+		button.setBackground(background);
+	}
+
+	/**
+	 * Sets the font that the receiver will use to paint textual information to the
+	 * font specified by the argument, or to the default font for that kind of
+	 * control if the argument is null.
+	 *
+	 * @param font the new font (or null)
+	 *
+	 * @exception IllegalArgumentException
+	 *                                     <ul>
+	 *                                     <li>ERROR_INVALID_ARGUMENT - if the
+	 *                                     argument has been disposed</li>
+	 *                                     </ul>
+	 * @exception SWTException
+	 *                                     <ul>
+	 *                                     <li>ERROR_WIDGET_DISPOSED - if the
+	 *                                     receiver has been disposed</li>
+	 *                                     <li>ERROR_THREAD_INVALID_ACCESS - if not
+	 *                                     called from the thread that created the
+	 *                                     receiver</li>
+	 *                                     </ul>
+	 */
+	public void setFont(Font font) {
+		checkWidget();
+		if (font == null) {
+			SWT.error(SWT.ERROR_NULL_ARGUMENT);
+		}
+		button.setFont(font);
+	}
+
+	/**
+	 * Sets the receiver's foreground color to the color specified by the argument,
+	 * or to the default system color for the control if the argument is null.
+	 * <p>
+	 * Note: This operation is a hint and may be overridden by the platform.
+	 * </p>
+	 * 
+	 * @param color the new color (or null)
+	 *
+	 * @exception IllegalArgumentException
+	 *                                     <ul>
+	 *                                     <li>ERROR_INVALID_ARGUMENT - if the
+	 *                                     argument has been disposed</li>
+	 *                                     </ul>
+	 * @exception SWTException
+	 *                                     <ul>
+	 *                                     <li>ERROR_WIDGET_DISPOSED - if the
+	 *                                     receiver has been disposed</li>
+	 *                                     <li>ERROR_THREAD_INVALID_ACCESS - if not
+	 *                                     called from the thread that created the
+	 *                                     receiver</li>
+	 *                                     </ul>
+	 */
+	public void setForeground(Color foreground) {
+		checkWidget();
+		if (foreground == null) {
+			SWT.error(SWT.ERROR_NULL_ARGUMENT);
+		}
+		button.setForeground(foreground);
+	}
+
+	/**
+	 * @see org.eclipse.swt.widgets.Item#setImage(org.eclipse.swt.graphics.Image)
+	 */
+	@Override
+	public void setImage(Image image) {
+		checkWidget();
+		button.setImage(image);
+		parent.layout(new Control[] { button });
+	}
+
+	/**
+	 * @see org.eclipse.swt.widgets.Item#setText(java.lang.String)
+	 */
+	@Override
+	public void setText(String string) {
+		checkWidget();
+		button.setText(string);
+		parent.layout(new Control[] { button });
+	}
+}
---
+++ bundles/com.tlcsdm.tlstudio.widgets/src/com/tlcsdm/tlstudio/widgets/viewers/RadioGroupViewer.java
@@ -0,0 +1,323 @@
+package com.tlcsdm.tlstudio.widgets.viewers;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.viewers.AbstractListViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Listener;
+
+import com.tlcsdm.tlstudio.widgets.custom.RadioGroup;
+import com.tlcsdm.tlstudio.widgets.custom.RadioItem;
+
+/**
+ * A concrete viewer based on a Nebula {@link RadioGroup} control.
+ * <p>
+ * <b>PROVISIONAL</b>: The superclass hierarchy of this viewer is provisional
+ * and expected to change. Specifically we plan to change this class to extend a
+ * new ItemViewer class in the near future. Therefore clients should avoid using
+ * API declared only in AbstractListViewer (however APIs declared in
+ * StructuredViewer and its superclasses are safe to use). Unsafe methods are
+ * tagged "noreference" in the javadoc.
+ *
+ * @since 3.5
+ */
+public class RadioGroupViewer extends AbstractListViewer {
+	private final RadioGroup radioGroup;
+
+	/**
+	 * Creates a radio group viewer on a newly-created {@link RadioGroup} control
+	 * under the given parent. The viewer has no input, no content provider, a
+	 * default label provider, no sorter, and no filters.
+	 *
+	 * @param parent the parent control
+	 */
+	public RadioGroupViewer(Composite parent) {
+		this(parent, SWT.NONE);
+	}
+
+	/**
+	 * Creates a radio group viewer on a newly-created {@link RadioGroup} control
+	 * under the given parent. The radio group control is created using the given
+	 * style bits. The viewer has no input, no content provider, a default label
+	 * provider, no sorter, and no filters.
+	 *
+	 * @param parent the parent control
+	 * @param style  SWT style bits
+	 */
+	public RadioGroupViewer(Composite parent, int style) {
+		this(new RadioGroup(parent, style));
+	}
+
+	/**
+	 * Creates a radio group viewer on the given {@link RadioGroup} control. The
+	 * viewer has no input, no content provider, a default label provider, no
+	 * sorter, and no filters.
+	 *
+	 * @param group the RadioGroup control
+	 */
+	public RadioGroupViewer(RadioGroup group) {
+		Assert.isNotNull(group);
+		radioGroup = group;
+		hookControl(group);
+
+		// Fix bug 430114
+		final Listener[] keyDownListeners = group.getListeners(SWT.KeyDown);
+		if (keyDownListeners.length == 1) {
+			group.removeListener(SWT.KeyDown, keyDownListeners[0]);
+		}
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public void add(Object element) {
+		super.add(element);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public void add(Object... elements) {
+		super.add(elements);
+	}
+
+	/**
+	 * @see org.eclipse.jface.viewers.Viewer#getControl()
+	 */
+	@Override
+	public Control getControl() {
+		return radioGroup;
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public Object getElementAt(int index) {
+		return super.getElementAt(index);
+	}
+
+	/**
+	 * Returns this viewer's {@link RadioGroup} control.
+	 *
+	 * @return the RadioGroup control
+	 */
+	public RadioGroup getRadioGroup() {
+		return radioGroup;
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected int indexForElement(Object element) {
+		return super.indexForElement(element);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public void insert(Object element, int position) {
+		super.insert(element, position);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listAdd(String string, int index) {
+		radioGroup.setLayoutDeferred(true);
+		try {
+			final RadioItem item = new RadioItem(radioGroup, SWT.NONE, index);
+			item.setText(string);
+		} finally {
+			radioGroup.setLayoutDeferred(false);
+		}
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listDeselectAll() {
+		radioGroup.deselectAll();
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected int listGetItemCount() {
+		return radioGroup.getItemCount();
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected int[] listGetSelectionIndices() {
+		return new int[] { radioGroup.getSelectionIndex() };
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected int listGetTopIndex() {
+		return super.listGetTopIndex();
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listRemove(int index) {
+		radioGroup.remove(index);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listRemoveAll() {
+		radioGroup.removeAll();
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listSetItem(int index, String string) {
+		final RadioItem item = radioGroup.getItems()[index];
+		item.setText(string);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listSetItems(String[] labels) {
+		radioGroup.removeAll();
+
+		for (int i = 0; i < labels.length; i++) {
+			final RadioItem item = new RadioItem(radioGroup, SWT.NONE);
+			item.setText(labels[i]);
+		}
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listSetSelection(int[] ixs) {
+		for (int idx = 0; idx < ixs.length; idx++) {
+			radioGroup.select(ixs[idx]);
+		}
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listSetTopIndex(int index) {
+		super.listSetTopIndex(index);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	protected void listShowSelection() {
+		// Do nothing -- selection visibility is determined by layout
+		// TODO - walk up parent hierarchy until we find a scrollable?
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public void remove(Object element) {
+		super.remove(element);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public void remove(Object... elements) {
+		super.remove(elements);
+	}
+
+	/**
+	 * @noreference Methods declared by AbstractListViewer should not be used by
+	 *              clients. This class will be modified to extend ItemViewer at
+	 *              some point in the future which will break references to
+	 *              AbstractListViewer API.
+	 */
+	@Override
+	public void reveal(Object element) {
+		// Do nothing -- element visibility is determined by layout
+		// TODO - walk up parent hierarchy until we find a scrollable?
+	}
+}
